5dc034abb2420c7f2779167c278b3d8cd3919bb8
LN77777 <137358642+LN77777@users.noreply.github.com>
2023-07-18 21:36:34 -0700

    Updated URL

--- api/graph.js
+++ api/graph.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     try {
-        const response = await axios.get('https://subgraph.satsuma-prod.com/realm/realm/playground');
+        const response = await axios.get('https://subgraph.satsuma-prod.com/9729d24721a2/realm/realm/api');
         res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data' });
