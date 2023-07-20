2334a2d7b1e92a2ccafeec2f3fd4c25013f605e6
LN77777 <137358642+LN77777@users.noreply.github.com>
2023-07-20 14:07:37 -0700

    Changed get to post

--- api/graph.js
+++ api/graph.js
@@ -2,9 +2,11 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     try {
-        const response = await axios.get('https://subgraph.satsuma-prod.com/9729d24721a2/realm/realm/api');
+        const response = await axios.post('https://subgraph.satsuma-prod.com/9729d24721a2/realm/realm/api');
         res.status(200).json(response.data);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Error fetching data' });
     }
+
 };
