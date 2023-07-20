1d76c9b3f58809ba0ab8954c7cddf92dedb5a755
LN77777 <137358642+LN77777@users.noreply.github.com>
2023-07-20 14:15:36 -0700

    added cors middleware

--- api/graph.js
+++ api/graph.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
+const cors = require('cors');
 
-module.exports = async (req, res) => {
-    try {
-        const response = await axios.post('https://subgraph.satsuma-prod.com/9729d24721a2/realm/realm/api');
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error fetching data' });
-    }
-
-};
+module.exports = cors((req, res) => {
+    axios.post('https://subgraph.satsuma-prod.com/9729d24721a2/realm/realm/api', req.body)
+        .then(response => {
+            res.json(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ error: 'Error fetching data' });
+        });
+});
